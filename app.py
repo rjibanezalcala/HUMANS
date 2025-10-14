b42adf4647f4404e5899af80e5fb0e1fae8d745b
Lara Rakocevic <lrakocev@gmail.com>
2025-10-14 15:11:20 -0400

    mkdir

--- app.py
+++ app.py
@@ -793,7 +793,6 @@ def write_userdata_to_file(user_id, filename, user_data, end_line='\n', include_
         
     data = deepcopy(user_data)
     filepath = f"data/{user_id}/{filename}"
-    os.mkdir(f"data/{user_id}")
 
     expected_formats = ['records', 'raw']
     
