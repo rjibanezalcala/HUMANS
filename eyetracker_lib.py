ab48b4f19263657c56e83a337fe2348a0537d2a0
Lara Rakocevic <lrakocev@gmail.com>
2025-12-02 12:24:52 -0500

    reqs

--- eyetracker_lib.py
+++ eyetracker_lib.py
@@ -5,7 +5,7 @@ eyetracker_lib.py v0.2 (updated October 27, 2025)
 @author: Raquel Ibáñez Alcalá
 """
 
-import tobii_research as tr
+#import tobii_research as tr
 from time import sleep
 from os import path
 from subprocess import run
